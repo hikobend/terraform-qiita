063bdcdb5a99e99e5c5bca9e1b4f3e953d42475b
hikobend <hiko.bkend@gmail.com>
2023-09-09 23:46:48 +0900

    メモ追加

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -8,11 +8,11 @@ module "network" {
   private_subnets         = ["10.0.0.128/26", "10.0.0.192/26"]
   public_subnet_names     = ["public-subnet-1a", "public-subnet-1c"]
   private_subnet_names    = ["private-subnet-1a", "private-subnet-1c"]
-  enable_dns_hostnames    = true
-  enable_dns_support      = true
-  enable_nat_gateway      = true
-  one_nat_gateway_per_az  = true
-  map_public_ip_on_launch = true
+  enable_dns_hostnames    = true # DNSホスト名を有効化
+  enable_dns_support      = true # DNS解決を有効化
+  enable_nat_gateway      = true # NatGatewayを有効化
+  one_nat_gateway_per_az  = true # 1AZに対してNatGatewayを作成
+  map_public_ip_on_launch = true # publicサブネットにIPv4を自動割り当て流
 
   vpc_tags                 = { Name = "vpc" }
   public_route_table_tags  = { Name = "route-table-public" }
