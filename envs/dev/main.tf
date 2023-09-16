6ad079bc08c862c209f2178b37c371820c47af3d
hikobend <hiko.bkend@gmail.com>
2023-09-16 18:02:01 +0900

    動作確認

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -2,10 +2,6 @@ module "dev_tfstate" {
   source = "../../modules/tfstate"
 }
 
-# module "network" {
-#   source = "../../modules/network"
-# }
-
-resource "aws_vpc" "name" {
-  cidr_block = "10.0.0.0/16"
+module "network" {
+  source = "../../modules/network"
 }
