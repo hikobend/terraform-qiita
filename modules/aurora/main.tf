154f9dcf2bcd7b16a914d690e31946556743cfe3
hikobend <hiko.bkend@gmail.com>
2023-09-30 23:39:38 +0900

    ログ取得

--- modules/aurora/main.tf
+++ modules/aurora/main.tf
@@ -6,3 +6,29 @@ resource "aws_db_subnet_group" "this" {
     Name = "${var.env}-aurora-subnet-group"
   }
 }
+
+resource "aws_db_parameter_group" "this" {
+  name   = "${var.env}-aurora-parameter-group"
+  family = "aurora-mysql8.0"
+
+  parameter {
+    name  = "general_log"
+    value = "1"
+  }
+
+  parameter {
+    name  = "slow_query_log"
+    value = "1"
+  }
+
+  parameter {
+    name  = "long_query_time"
+    value = "0"
+  }
+
+  parameter {
+    name         = "log_output"
+    value        = "FILE"
+    apply_method = "pending-reboot"
+  }
+}
